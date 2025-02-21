1ac94592305aad1117b59663367593ca4aa30428
AnthonnyLac <91605907+AnthonnyLac@users.noreply.github.com>
2025-02-20 21:07:56 -0300

    refactor: altera forma de buscar dll python

--- LangSharp.Out/Program.cs
+++ LangSharp.Out/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LangSharp.Out.Core;
+using LangSharp.Out.Demo;
 
 namespace LangSharp.Out
 {
@@ -6,54 +7,17 @@ namespace LangSharp.Out
     {
         static void Main(string[] args)
         {
+
             ISDKService sdkFacade = new SDKFacade();
 
-            Console.WriteLine("Digite um comando Python:");
-            string command = Console.ReadLine();
+            //Console.WriteLine("Digite um comando Python:");
+            //string command = Console.ReadLine();
 
-            string result = sdkFacade.ExecutePythonCommand(command);
-            Console.WriteLine($"Resultado: {result}");
-
-
-            //// Cria uma instância da classe PythonInterop
-            //using var pythonInterop = new PythonInterop();
+            //string result = sdkFacade.ExecutePythonCommand(command);
+            //Console.WriteLine($"Resultado: {result}");
 
 
-            //try
-            //{
-            //    // Inicializa o ambiente Python
-            //    pythonInterop.InitializePython();
-
-            //    // Código Python para testar
-            //    string pythonCode = "print('sim')";
-
-            //    // Executa o código Python
-            //    pythonInterop.ExecutePythonCode(pythonCode);
-
-            //    //Dados base
-            //    var scriptName = "LangSharp.py";
-            //    var moduleName = "LangSharp";
-            //    var methodName = "my_function";
-            //    var argsPython = new object[] { 1, 2 };
-
-
-            //    // Executa o código Python
-            //    pythonInterop.ExecutePythonScript(scriptName, moduleName, methodName, argsPython);
-
-            //    //Teste
-            //    pythonInterop.CreateVirtualEnvironment();
-            //    pythonInterop.InstallPythonPackage("langchain");
-
-            //}
-            //catch (Exception ex)
-            //{
-            //    Console.WriteLine($"Ocorreu um erro: {ex.Message}");
-            //}
-            //finally
-            //{
-            //    // Libera recursos do Python
-            //    pythonInterop.Dispose();
-            //}
+            InityPy.StartDemo();
         }
     }
 }
